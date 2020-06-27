a568cd5a85f51e6f8a4f0071a40b27388969aecf
Saurabh Khandelwal <khandelwal.saurabh31@gmail.com>
2020-06-27 14:56:57 +0530

    Create TEST.cpp
    Link to Problem Statement - https://www.spoj.com/problems/TEST/

---
+++ TEST.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int i = 0;
+    cin>>i;
+    while(i!=42){
+        cout<<i<<endl;
+        cin>>i;
+    }
+	return 0;
+}
